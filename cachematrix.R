ba762f66154218afb6614a15dc97f19ed303fd72
huw williams <huw_williams@mac.com>
2015-08-21 13:47:07 +0400

    First Push
    First push, may make more comments lated

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cache solve makes a matrix cache
+## that can be checked by
+## subsequent computations
+##
+##
+##
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(passed_data = matrix()) {
+    inv_passed_data <- NULL ## clear before use
+    set <- function(y) {
+        passed_data <<- y ## assign outside of function
+        inv_passed_data <<- NULL ## clear before use
+    }
+    get <- function() passed_data
+    setinverse<- function(inverse) inv_passed_data <<-inverse
+    getinverse <- function() inv_passed_data
+    list(set = set, get = get, ## as in demo function
+    setinverse = setinverse,
+    getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(passed_data, ...) {
+    
+    ## Return a matrix that is the inverse of 'x'
+    inv_x <- passed_data$getinverse()
+    if (!is.null(inv_x)) {
+        message("getting cached inverse matrix")
+        return(inv_x)
+    } else {
+        inv_x <- solve(passed_data$get())
+        passed_data$setinverse(inv_x)
+        return(inv_x)
+    }
 }
+
+test = function(mat){
+    
+    ## to use
+    ## set.seed(1110201)
+    ##mat1 = matrix(r, nrow=1000, ncol=1000)
+    ##r = rnorm(1000000)
+    ##test(mat1)
+    ## @mat: an invertible matrix
+    ##
+    print('Test Starts');
+    temp = makeCacheMatrix(mat)
+    
+    start.time = Sys.time()
+    cacheSolve(temp)
+    dur = Sys.time() - start.time
+    print(dur)
+    
+    start.time = Sys.time()
+    cacheSolve(temp)
+    dur = Sys.time() - start.time
+    print(dur)
+}
+
+
+
